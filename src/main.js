d3da4e3431ce2675bbf447e5720084cbe9c55b1b
zh-vah <1991249027@qq.com>
2020-12-07 18:06:45 +0800

    梁场静态页面

--- src/main.js
+++ src/main.js
@@ -8,15 +8,21 @@ import BoxRight from '@/components/BoxRight.vue'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/reset.sass'
 
+import ECharts from 'vue-echarts'
+import 'echarts/lib/chart/line'
+import 'echarts/lib/chart/bar'
+import 'echarts/lib/chart/pie'
+import 'echarts/lib/component/legend'
+import 'echarts/lib/component/tooltip'
+
+Vue.component('v-chart', ECharts)
+
+
 Vue.use(ElementUI)
 Vue.use(BoxLeft)
+Vue.component('BoxLeft', BoxLeft)
 Vue.component('BoxRight', BoxRight)
-    // const loading = {
-    //     install(Vue, options) {
-    //         Vue.component('BoxLeft', BoxLeft)
-    //         Vue.component('BoxRight', BoxRight)
-    //     }
-    // }
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/liangchang/liangchangIndex.vue
+++ src/views/liangchang/liangchangIndex.vue
@@ -1,16 +1,30 @@
 <template>
   <div>
-    <BoxLeft class="boxleft" title="质量巡检">
-      <p class="text">
-            苏台高速公路是“长三角经济圈”苏浙两省间六条大通道之一，是长三角高速公路网规划
-        的“纵三”江苏盐城至浙江绍兴高速公路的重要组成部分，起自苏州市，由江苏省苏震桃
-        高速公路进入浙江省，经湖州、嘉兴，接已建成通车的钱江通道及其南北接线，通过杭绍
-        台高速公路，直达台州。苏台高速公路路线全长约34.674km（湖州南浔区长12.929km，嘉
-        兴桐乡市长21.745km.主线：设计速度120km/h；双向六车道高速公路，路基宽度为34.5米；
-        联络线：设计速度100km/h，双向六车道高速公路，路基宽33.5米。
-      </p>
+    <BoxLeft title="项目概况">
+      <div class="left-content">
+          <p class="text" v-text="text1"></p>
+        <div class="roam">
+            <div class="left-icon"><i class="icon iconfont icon-kaishi"></i></div>
+            <p class="roma-text">开始厂区漫游1</p>
+        </div>
+       <div style="height: 200px;">
+          <v-chart :options="polar" :style="{width: '300px', height: '200px'}" />
+       </div>
+      </div>
     </BoxLeft>
-    <BoxRight title="质量巡检"></BoxRight>
+    <BoxRight title="梁场概况">
+      <div class="right-content">
+          <video src="" class="video"></video>
+          <div class="right-icon"><i class="icon iconfont icon-kaishi right"></i></div>
+          <p class="text" v-text="text2"></p>
+          <div class="subbox">
+              <img class="subbox-img" src="../../assets/img/BoxleftIcon.png" alt="">
+              <h3 class="subbox-title">原材料控制</h3>
+              <p class="subbox-text">原材料管控的对接ababab公路是“长三角经济圈”苏浙两省间六条大通道之一，是长三角高速公路网规划的“纵三”江苏盐城至浙江绍兴高速公路的重要组成部分，起自苏州市，由江苏省苏震桃高速公路进入浙江省，经湖州、嘉兴，接已建成通车的钱江通道及其南北接线，通过杭绍"+
+             "台高速公路，直达台州。苏</p>
+          </div >
+      </div>
+    </BoxRight>
   </div>
   
 </template>
@@ -18,17 +32,117 @@
 <script>
 export default {
   name: "investmentControl",
+  data(){
+    return {
+      polar: {
+          tooltip: {
+            trigger: 'item',
+            formatter: '{a} <br/>{b}: {c} ({d}%)'
+        },
+        legend: {
+        },
+        series: [
+            {
+                name: '访问来源',
+                type: 'pie',
+                radius: '40%',
+                center: ['50%', '60%'],
+                data: [
+                    {value: 88, name: '梁板总数'},
+                    {value: 11, name: '已生产数'},
+                ],
+                label: {
+                    formatter:'{d}%'
+                },
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+      },
+      text1: "苏台高速公路是“长三角经济圈”苏浙两省间六条大通道之一，是长三角高速公路网规划的“纵三”江苏盐城至浙江绍兴高速公路的重要组成部分，起自苏州市，由江苏省苏震桃高速公路进入浙江省，经湖州、嘉兴，接已建成通车的钱江通道及其南北接线，通过杭绍"+
+             "台高速公路，直达台州。苏台高速公路路线全长约34.674km（湖州南浔区长12.929km，嘉兴桐乡市长21.745km.主线：设计速度120km/h；双向六车道高速公路，路基宽度为34.5米；联络线：设计速度100km/h，双向六车道高速公路，路基宽33.5米。",
+      text2: "智慧梁厂应用5G、物联网、BIM/GIS等信息化技术手段，围绕信息化、智能化、可视化的项目管理理念，将传统梁厂的生产要素进行数字化处理，业务流程通过系统自动上传下达，" +
+              "充分开发利用信息资源，促进信息交流和知识共享，以信息化建设推动梁厂的管理水平和生产效率的提升"
+    }
+  },
+ mounted(){
+   this.drawLine();
+ },
+  methods:{
+    drawLine(){
+        
+    }
+  }
 };
 </script>
 
 <style lang="sass" scoped>
-.boxleft
+.left-content
   .text
+    line-height: 26px;
     font-size: 14px;
     color: rgb( 255, 255, 255 );
     text-align: left;
-    width: 295px;
-    height: 281px;
-
-
+    text-indent: 2em;
+  .roam
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    weight: 299px;
+    height: 38px;
+    margin-top: 10px;
+    padding-top: 10px;
+    background: url('../../assets/img/roma-background.png') no-repeat
+    cursor: pointer
+    &:hover
+      opacity: .9
+    .left-icon
+      margin-right: 7px;
+    .rima-text
+      font-size: 18px;
+.right-content
+  .video
+    width: 375px;
+    height: 175px;
+    margin-left: 10px;
+    margin-top: 18px;
+    background-color: #0a2f46;
+  .right-icon
+    position: absolute;
+    left: 191px;
+    top: 77px;
+    .right
+      font-size: 50px;
+      color: #097dc2
+  .text
+    line-height: 26px;
+    font-size: 14px;
+    color: rgb( 255, 255, 255 );
+    text-align: left;
+    text-indent: 2em;
+  .subbox
+    position: relative;
+    width: 376px;
+    margin-top: 5px;
+    background-color: rgba(11,46,67,.8);
+    .subbox-img
+      margin: 5px 0 12px 17px
+    .subbox-title
+      position: absolute;
+      left: 46px;
+      top: 5px;
+      font-size: 16px;
+      color: rgb( 38, 206, 255 );
+    .subbox-text
+      color: rgb( 255, 255, 255 );
+      font-size: 14px;
+      text-indent: 2em;
+      line-height: 25px;
 </style>
+
+
